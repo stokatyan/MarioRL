4ada555e92c30e96eca63034bb334e8f046e4f4d
Shant Tokatyan <shanttokatyan@system76-pc.localdomain>
2020-04-01 13:32:16 -0700

    Converted SACAgent.py into static functions instead of script; uses Playback uses stati funcitons from SACAgent.py to playback best checkpoint

--- Playback.py
+++ Playback.py
@@ -21,6 +21,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 import MarioEnvironment
+import SACAgent
 
 
 physical_devices = tf.config.list_physical_devices('GPU') 
@@ -31,93 +32,13 @@ except:
   pass 
 
 
-num_iterations = 100000 # @param {type:"integer"}
-
-initial_collect_steps = 1000 # @param {type:"integer"} 
-collect_steps_per_iteration = 1 # @param {type:"integer"}
-replay_buffer_capacity = 10000 # @param {type:"integer"}
-
-batch_size = 150 # @param {type:"integer"}
-
-critic_learning_rate = 3e-4 # @param {type:"number"}
-actor_learning_rate = 3e-4 # @param {type:"number"}
-alpha_learning_rate = 3e-4 # @param {type:"number"}
-target_update_tau = 0.005 # @param {type:"number"}
-target_update_period = 1 # @param {type:"number"}
-gamma = 0.99 # @param {type:"number"}
-reward_scale_factor = 1.0 # @param {type:"number"}
-gradient_clipping = None # @param
-
-actor_fc_layer_params = (50, 50)
-critic_joint_fc_layer_params = (50, 50)
-
-log_interval = 500 # @param {type:"integer"}
-
-num_eval_episodes = 5 # @param {type:"integer"}
-eval_interval = 2000 # @param {type:"integer"}
-
-
-train_py_env = MarioEnvironment.MarioEnvironment()
-eval_py_env = MarioEnvironment.MarioEnvironment()
-
-train_env = tf_py_environment.TFPyEnvironment(train_py_env)
-eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
-
-observation_spec = train_env.observation_spec()
-action_spec = train_env.action_spec()
-critic_net = critic_network.CriticNetwork(
-    (observation_spec, action_spec),
-    observation_fc_layer_params=None,
-    action_fc_layer_params=None,
-    joint_fc_layer_params=critic_joint_fc_layer_params)
-
-
-def normal_projection_net(action_spec,init_means_output_factor=0.1):
-  return normal_projection_network.NormalProjectionNetwork(
-      action_spec,
-      mean_transform=None,
-      state_dependent_std=True,
-      init_means_output_factor=init_means_output_factor,
-      std_transform=sac_agent.std_clip_transform,
-      scale_distribution=True)
-
-
-actor_net = actor_distribution_network.ActorDistributionNetwork(
-    observation_spec,
-    action_spec,
-    fc_layer_params=actor_fc_layer_params,
-    continuous_projection_net=normal_projection_net)
-
-
-global_step = tf.compat.v1.train.get_or_create_global_step()
-tf_agent = sac_agent.SacAgent(
-    train_env.time_step_spec(),
-    action_spec,
-    actor_network=actor_net,
-    critic_network=critic_net,
-    actor_optimizer=tf.compat.v1.train.AdamOptimizer(
-        learning_rate=actor_learning_rate),
-    critic_optimizer=tf.compat.v1.train.AdamOptimizer(
-        learning_rate=critic_learning_rate),
-    alpha_optimizer=tf.compat.v1.train.AdamOptimizer(
-        learning_rate=alpha_learning_rate),
-    target_update_tau=target_update_tau,
-    target_update_period=target_update_period,
-    td_errors_loss_fn=tf.compat.v1.losses.mean_squared_error,
-    gamma=gamma,
-    reward_scale_factor=reward_scale_factor,
-    gradient_clipping=gradient_clipping,
-    train_step_counter=global_step)
+tf_agent = SACAgent.create_agent()
 tf_agent.initialize()
 
-
+eval_env = SACAgent.eval_env
 eval_policy = greedy_policy.GreedyPolicy(tf_agent.policy)
 
-replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
-    data_spec=tf_agent.collect_data_spec,
-    batch_size=train_env.batch_size,
-    max_length=replay_buffer_capacity)
-
+replay_buffer = SACAgent.create_replay_buffer(tf_agent)
 
 checkpoint_dir = "checkpoint"
 train_checkpointer = common.Checkpointer(
@@ -126,7 +47,7 @@ train_checkpointer = common.Checkpointer(
     agent=tf_agent,
     policy=tf_agent.policy,
     replay_buffer=replay_buffer,
-    global_step=global_step
+    global_step=SACAgent.global_step
 )
 
 train_checkpointer.initialize_or_restore()
